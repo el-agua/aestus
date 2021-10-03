e19a25ba3cd228b7052df794321fb3193152e959
DD Z <68676272+el-agua@users.noreply.github.com>
2021-10-03 09:49:59 -0500

    testestestst

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
 import Head from "next/head";
 import DeckGL from "@deck.gl/react";
-import { LineLayer } from "@deck.gl/layers";
+import { LineLayer, ScatterplotLayer} from "@deck.gl/layers";
+import {HexagonLayer} from '@deck.gl/aggregation-layers';
 import { StaticMap } from "react-map-gl";
+const MALE_COLOR = [0, 128, 255];
+const FEMALE_COLOR = [255, 0, 128];
+
+// Source data CSV
+var json = require("../Proc-data.json");
+
 const INITIAL_VIEW_STATE = {
   longitude: -122.41669,
   latitude: 37.7853,
@@ -19,8 +26,43 @@ const data = [
   },
 ];
 
-export default function Home({ data }) {
-  const layers = [new LineLayer({ id: "line-layer", data })];
+
+
+export default function Home() {
+  return (
+    <div>
+      <div>Hello</div>
+      <Map />
+    </div>
+  );
+}
+function Map({
+  data = json,
+  radius = 30,
+  mapStyle = 'https://basemaps.cartocdn.com/gl/positron-nolabels-gl-style/style.json'
+}) {
+  
+  const layers = [new HexagonLayer({
+    id: 'heatmap',
+    colorRange: [
+      [1, 152, 189],
+      [73, 227, 206],
+      [216, 254, 181],
+      [254, 237, 177],
+      [254, 173, 84],
+      [209, 55, 78]
+    ],
+    coverage: 1,
+    data,
+    elevationRange: [0, 10000],
+    elevationDomain: [280, 300],
+    getElevationWeight: d => d.TEMPERATURE,
+    extruded: true,
+    getPosition: d => [d.LATLON[1],d.LATLON[0]],
+    pickable: true,
+    radius: 10000,
+    upperPercentile: 100,
+  })];
   return (
     <DeckGL
       initialViewState={INITIAL_VIEW_STATE}
@@ -28,7 +70,6 @@ export default function Home({ data }) {
       layers={layers}
     >
       <StaticMap
-        mapStyle="mapbox://styles/mapbox/satellite-v9"
         mapboxApiAccessToken={MAPBOX_ACCESS_TOKEN}
       />
     </DeckGL>
